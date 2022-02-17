3cde77920d086f7ca622db40ae5971a585396823
John Baumert <36886175+baumertjohn@users.noreply.github.com>
2022-02-17 05:56:27 -0700

    Added blocks and scoreboard

--- assets.py
+++ assets.py
@@ -2,20 +2,22 @@ from turtle import Turtle
 
 PADDLE_WIDTH = 7
 PADDLE_HEIGHT = 1
-PADDLE_SPEED = 20
+PADDLE_SPEED = 50
+
+SB_FONT = ('Courier', 24, 'bold')
 
 
+########## Paddle ##########
 class Paddle(Turtle):
-    def __init__(self, y_start):
+    def __init__(self):
         super().__init__()
-        self.y_start = y_start
         self.create_paddle()
 
     def create_paddle(self):
         self.shape('square')
         self.color('blue')
         self.penup()
-        self.goto(0, self.y_start)
+        self.goto(0, -270)
         self.shapesize(PADDLE_HEIGHT, PADDLE_WIDTH, None)
 
     def paddle_left(self):
@@ -31,8 +33,9 @@ class Paddle(Turtle):
             self.goto(x_position + PADDLE_SPEED, y_position)
 
 
+########## Ball ##########
 class Ball(Turtle):
-    def __init__(self, ball_speed=0.1):
+    def __init__(self, ball_speed=2):
         super().__init__()
         self.ball_speed = ball_speed
         self.create_ball()
@@ -58,10 +61,64 @@ class Ball(Turtle):
         self.ball_y_travel *= -1
 
     def reset_ball(self):
-        self.goto(0, 0)
+        self.goto(0, -200)
         self.top_bottom_bounce()
 
 
-class Block(Turtle):
+########## Block ##########
+class BlockManager():
+    def __init__(self):
+        # super().__init__()
+        self.blocklist = []
+        self.start_game()
+
+    def create_block(self, x_pos, y_pos, color):
+        block = Turtle(shape='square')
+        block.shapesize(1, 3, 1)
+        block.penup()
+        block.color(color)
+        block.goto(x_pos, y_pos)
+        self.blocklist.append(block)
+
+    def start_game(self):
+        block_colors = ['red', 'orange', 'yellow', 'green', 'blue']
+        y_pos = 225
+        for color in range(5):
+            x_pos = -360
+            for _ in range(11):
+                self.create_block(x_pos, y_pos, block_colors[color])
+                x_pos += 71
+            y_pos -= 35
+
+    def extra_block(self):
+        # Hack to keep the game speed consistent
+        block = Turtle(shape='square')
+        block.shapesize(1, 3, 1)
+        block.penup()
+        block.color('black')
+        block.goto(1000, 1000)
+        self.blocklist.append(block)
+
+
+########## Scoreboard ##########
+class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        self.color('white')
+        self.penup()
+        self.hideturtle()
+        self.score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(0, 300)
+        self.write(f'SCORE: {self.score}', align='center', font=SB_FONT)
+
+    def increase_score(self):
+        self.score += 10
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write('GAME OVER', align='center', font=SB_FONT)
--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from turtle import Screen
-from assets import Paddle, Ball, Block
+from assets import Paddle, Ball, BlockManager, Scoreboard
 
-GAME_SPEED = 0.15
+GAME_SPEED = 1.5
 
 # Game screen setup
 screen = Screen()
-screen.setup(width=800, height=600)
+screen.setup(width=800, height=700)
 screen.bgcolor('black')
 screen.title('BREAKOUT')
 screen.tracer(0)  # Reduce flicker
 
-paddle = Paddle(-270)
+paddle = Paddle()
 ball = Ball(GAME_SPEED)
+blockmanager = BlockManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkeypress(paddle.paddle_left, 'Left')
@@ -20,8 +22,6 @@ screen.onkeypress(paddle.paddle_right, 'Right')
 game_active = True
 
 while game_active:
-    screen.update()
-
     # Move ball
     ball.move_ball()
 
@@ -34,11 +34,23 @@ while game_active:
         ball.top_bottom_bounce()
 
     # Detect paddle collision
-    if ball.distance(paddle) < 50 and ball.ycor() > -280:
+    if ball.distance(paddle) < 50 and ball.ycor() < -250:
         ball.top_bottom_bounce()
 
     # Detect paddle miss
     if ball.ycor() < -300:
         ball.reset_ball()
+
+    # Detect block colision
+    if ball.ycor() > 0:
+        for count, block in enumerate(blockmanager.blocklist):
+            if block.distance(ball) < 40:
+                block.ht()  # "Hides" the turtle
+                del blockmanager.blocklist[count]  # Deletes block object
+                blockmanager.extra_block()
+                ball.top_bottom_bounce()
+                scoreboard.increase_score()
+                
+    screen.update()
 
 screen.exitonclick()
